a4025d9fb985c264cf158afbaae4042329bb067c
eristoddle <stephanmil@gmail.com>
2025-09-19 12:37:08 -0500

    Bump version to 0.2.0

--- mdquery/__init__.py
+++ mdquery/__init__.py
@@ -5,7 +5,7 @@ A SQL-like interface for searching and analyzing markdown files across
 different note-taking systems and static site generators.
 """
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 __author__ = "mdquery"
 __description__ = "Universal markdown querying tool with SQL-like syntax"
 
